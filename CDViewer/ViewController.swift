41987521921ed3b6b3215bf24928fd7939eb0fac
Użytkownik Gość <Guest@imac-10.local>
2017-10-10 10:55:24 +0200

    after first lab

--- CDViewer/ViewController.swift
+++ CDViewer/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var CDdata: [String: Any] = [:]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func getData(){
+        let urlString = URL(string: "https://isebi.net/albums.php")
+        if let url = urlString {
+            let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                } else {
+                    if let usableData = data {
+                        jsonObject(with data: data,
+                                   options opt: JSONSerialization.ReadingOptions = [])
+                        print(usableData) //JSONSerialization
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
 }
 
